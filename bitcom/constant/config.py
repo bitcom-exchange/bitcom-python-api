a854c0db814566eed38a7a90417a3c4b1eadcbf0
huahui.gao <huahui.gao@matrixport.com>
2021-02-05 09:36:41 +0800

    remove host & key

--- bitcom/constant/config.py
+++ bitcom/constant/config.py
@@ -1,14 +1,5 @@
-# wafei alpha
-USER1_HOST = "https://alphaapi.bitexch.dev"
-USER1_UID = "475183"
-USER1_ACCESS_KEY = "ak-3d4dc159-8313-4726-860e-60355ccf1d24"
-USER1_SECRET_KEY = "A2I2HuUKqx7VnxQVvJhvowvmgQ3GgunDtdsHHyEjZoQXn14zmf8hB8MZzjlgGOFg"
+USER1_HOST = "host"
+USER1_UID = "uid"
+USER1_ACCESS_KEY = "access-key"
+USER1_SECRET_KEY = "secret-key"
 
-
-# eric alpha
-USER2_HOST = "https://alphaapi.bitexch.dev"
-USER2_UID = "51140"
-USER2_ACCESS_KEY = "ak-96cc0cbd-c501-448f-a32d-21228bc9648f"
-USER2_SECRET_KEY = "IM2DHuw04GQSaS4SLnXlCf3zegw0Lgli0YwGXAcd3c1vEXYjFVBHz8aViUehRNji"
-
-WS_HOST = "wss://alphaws.bitexch.dev"
