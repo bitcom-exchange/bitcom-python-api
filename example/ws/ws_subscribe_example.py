6fbdcd1670819939c2c371640e1bd2ae4a5ff236
wafeishushu <a153255005@163.com>
2021-04-09 17:40:27 +0800

    Update ws_subscribe_example.py

--- example/ws/ws_subscribe_example.py
+++ example/ws/ws_subscribe_example.py
@@ -6,14 +6,14 @@ from bitcom.constant import *
 
 
 def on_wss_open(ws):
-    cmd = """{"type": "subscribe", "instruments": ["BTC-PERPETUAL"],
-     "channels": ["depth1"], "interval": "raw", "token": "13c8df77-038d-451b-9460-7de6c271efd8"}"""
+    cmd = {"type": "subscribe", "instruments": ["BTC-PERPETUAL"],
+           "channels": ["ticker"], "interval": "raw", "token": "13c8df77-038d-451b-9460-7de6c271efd8"}
 
-    # check json
-    obj = json.loads(cmd)
+    # convert to string
+    cmdStr = json.dumps(cmd)
 
-    print('send subscribe cmd: ' + cmd)
-    ws.send(cmd)
+    print('send subscribe cmd: ' + cmdStr)
+    ws.send(cmdStr)
 
 
 def on_wss_msg(ws, data):
