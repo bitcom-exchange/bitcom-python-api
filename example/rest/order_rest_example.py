2a6bdbe580e1d80ee383d27f51a8d73e2364cb5f
huahui.gao <huahui.gao@matrixport.com>
2021-04-16 16:55:45 +0800

    update examples

--- example/rest/order_rest_example.py
+++ example/rest/order_rest_example.py
@@ -4,16 +4,16 @@ from bitcom.constant import *
 
 order_client = OrderClient(url=USER1_HOST, access_key=USER1_ACCESS_KEY, secret_key=USER1_SECRET_KEY)
 
-# param_map = {
-#     'instrument_id': 'BTC-PERPETUAL',
-#     'qty': '1000',
-#     'side': 'buy',
-#     'price': '11000.50',
-#     'order_type': 'limit',
-#     'time_in_force': 'gtc',
-# }
-# new_order_response = order_client.place_new_order(param_map)
-# LogInfo.output("Place new order: ", new_order_response)
+param_map = {
+    'instrument_id': 'BTC-PERPETUAL',
+    'qty': '1000',
+    'side': 'buy',
+    'price': '11000.50',
+    'order_type': 'limit',
+    'time_in_force': 'gtc',
+}
+new_order_response = order_client.place_new_order(param_map)
+LogInfo.output("Place new order: ", new_order_response)
 
 
 param_map = {
